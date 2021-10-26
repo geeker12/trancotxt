318b219c91821b25489cf893014d173303b4b85c
geeker12 <45853278+geeker12@users.noreply.github.com>
2021-10-26 12:13:05 +0100

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,5 +1,6 @@
 from tranco import Tranco
 import dns.resolver  
+import csv
 import os
 os.system('clear')
 from datetime import datetime, timedelta
@@ -16,24 +17,14 @@ def get_spf(domain):
       return answer.to_text()
     else:
       return "No SPF"
+header  = ['domain', 'SPF']
 for host  in list_1k:
   try:
-    print(host+','+get_spf(host))
+    with open('result.csv', 'a', encoding='UTF8') as f:
+     writer = csv.writer(f)
+     writer.writerow([host,get_spf(host)])
   except Exception as e:
-    print(e)
+    print(e.__doc__)
 
 
-# my_resolver = dns.resolver.Resolver()
 
-# for domain  in one_m_list:
-#   try:
-#     answer_txt = my_resolver.resolve(domain, "TXT")
-#     for TXT in answer_txt.rrset:
-#       if "spf" in str(TXT):
-#         result=domain+','+str(TXT).strip("\"")+'\n'
-#         with open ("out4.txt" , "a") as out_put:
-#           out_put.write(result)
-#           out_put.close()
-#       break    
-#   except Exception as e:
-#     print(domain + ' ' +str(e))
